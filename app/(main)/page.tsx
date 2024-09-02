104deb6ad7bcf8d0603395fcf47d65a527b5d65f
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-02 16:54:30 +0300

    auth

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <p>Convo app</p>
+      <UserButton />
     </div>
   );
 }
