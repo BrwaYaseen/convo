17155c2acc70e95cc44e16a714f5c0657e1c8bc2
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-07 15:05:56 +0300

    invite done

--- app/(invite)/invite/[inviteCode]/page.tsx
+++ app/(invite)/invite/[inviteCode]/page.tsx
@@ -1,9 +1,56 @@
-const InviteCodePage = () => {
-  return (
-    <div>
-      <p>Hello Bro</p>
-    </div>
-  );
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+
+const InviteCodePage = async ({
+  params,
+}: {
+  params: { inviteCode: string };
+}) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirect("/sign-in");
+  }
+  if (!params.inviteCode) {
+    return redirect("/");
+  }
+
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            profileId: profile.id,
+          },
+        ],
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return null;
 };
 
 export default InviteCodePage;
