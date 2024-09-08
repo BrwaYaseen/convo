fae82f97d05bd4addf4aa839d56a53221a871217
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-08 22:10:12 +0300

    channels part 2

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -16,19 +16,20 @@ type ServerSectionProps = {
 export const ServerSection = ({
   label,
   role,
+  channelType,
   sectionType,
   server,
 }: ServerSectionProps) => {
   const { onOpen } = useModal();
   return (
-    <div className="flex items-center justify-center py-2">
+    <div className="flex items-center justify-between py-2">
       <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
         {label}
       </p>
       {role !== MemberRole.GUEST && sectionType === "channels" && (
         <ActionTooltip label="Create Channel" side="top">
           <button
-            onClick={() => onOpen("createChannel")}
+            onClick={() => onOpen("createChannel", { channelType })}
             className="text-zinc-500 hover:text-zinc-600
            dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
@@ -37,7 +38,7 @@ export const ServerSection = ({
         </ActionTooltip>
       )}
       {role === MemberRole.ADMIN && sectionType === "members" && (
-        <ActionTooltip label="Members" side="top">
+        <ActionTooltip label="Manage Members" side="top">
           <button
             onClick={() => onOpen("members", { server })}
             className="text-zinc-500 hover:text-zinc-600
--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -22,7 +22,10 @@ export const initialProfile = async () => {
   const newProfile = await db.profile.create({
     data: {
       userId: user.id,
-      name: `${user.firstName} ${user.lastName}`,
+      name:
+        user.firstName && user.lastName
+          ? `${user.firstName} ${user.lastName}`
+          : user.username || user.fullName || "Unknown User",
       imageUrl: user.imageUrl,
       email: user.emailAddresses[0].emailAddress,
     },
