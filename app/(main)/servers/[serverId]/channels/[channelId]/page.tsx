870630f46c872a0a3fca0aec1aa613990f0ad54b
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-09 16:09:14 +0300

    chat header

--- app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,45 @@
-const ChannelIdPage = () => {
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+type ChannelIdPageProps = {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+};
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/sign-in");
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
   return (
-    <div>
-      <p>Channel Id Page</p>
+    <div className="flex flex-col bg-white dark:bg-[#313338]  h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
     </div>
   );
 };
