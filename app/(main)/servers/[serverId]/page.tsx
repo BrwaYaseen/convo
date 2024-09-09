1e3c29f11334e5a2ee5dbfac5584004a1932ed40
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-09 15:15:24 +0300

    directions

--- app/(main)/servers/[serverId]/page.tsx
+++ app/(main)/servers/[serverId]/page.tsx
@@ -1,9 +1,41 @@
-const ServerPage = () => {
-  return (
-    <div>
-      <p>Server ID Page</p>
-    </div>
-  );
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const ServerIdPage = async ({ params }: { params: { serverId: string } }) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/sign-in");
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 };
 
-export default ServerPage;
+export default ServerIdPage;
