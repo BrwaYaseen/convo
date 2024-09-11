a0219ea9c4483fb1ce8b191f6ee7cf236033b119
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-11 18:35:33 +0300

    chat item 1

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,6 +5,10 @@ import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import { ChatItem } from "./chat-item";
+import { format } from "date-fns";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 type ChatMessagesProps = {
   name: string;
@@ -66,11 +70,23 @@ export const ChatMessages = ({
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className=" flex-1" />
       <ChatWelcome type={type} name={name} />
-      <div className="flex flex-col-reverse ml-auto">
+      <div className="flex flex-col-reverse">
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>{message.content}</div>
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                content={message.content}
+                member={message.member}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                currentMember={member}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
